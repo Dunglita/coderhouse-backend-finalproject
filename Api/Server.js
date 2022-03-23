841b47417806e424b413680a68ff40469a66dee6
Dani <garciadanos@gmail.com>
2022-03-23 15:46:53 -0300

    Created server

---
+++ Api/Server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const getRoutes = require("./Routes/Routes.js");
+const app = express();
+
+class Server {
+  constructor() {
+    this.server = app;
+    getRoutes(app);
+  }
+
+  run(port) {
+    this.server.listen(port, (err) => {
+      if (err) {
+        process.exit(1);
+      }
+      console.log(`Server listening on port: ${port}`);
+    });
+  }
+}
+
+module.exports = Server;
