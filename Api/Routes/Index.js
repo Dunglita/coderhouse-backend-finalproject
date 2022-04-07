680220f6e7968c03e088d46bba1fb7d7f441969a
Dani <garciadanos@gmail.com>
2022-04-07 04:13:46 -0300

    added body parser config

--- Api/Routes/Index.js
+++ Api/Routes/Index.js
@@ -1,17 +1,24 @@
 const express = require(`express`);
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const compression = require("compression");
 const productRoutes = require("./Product.routes.js");
 const cartRoutes = require("./Cart.routes.js");
+const router = express();
 
 function getRoutes(app) {
   app
     .use(express.json())
     .use(cors({ credentials: true }))
     .use(compression())
-    .use(express.static("public"));
+    .use(express.static("public"))
+    .use(
+      bodyParser.urlencoded({
+        extended: true,
+      })
+    );
 
-  app.use("/api/productos", productRoutes);
-  app.use("/api/carrito", cartRoutes);
+  app.use("/api/productos", productRoutes(router));
+  app.use("/api/carrito", cartRoutes(router));
 }
 module.exports = getRoutes;
